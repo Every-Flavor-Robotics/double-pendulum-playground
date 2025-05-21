4a6a2392c7f5164e5ef2acfd7cdf36003a385cda
Swapnil Pande <swapnil.pande98@gmail.com>
2025-05-21 13:21:55 -0400

    Run multiple episodes, using new model loading

--- jax/eval.py
+++ jax/eval.py
@@ -2,6 +2,7 @@ import pathlib
 from functools import partial
 
 import imageio
+import numpy as np
 from flax.serialization import from_bytes
 from wrappers import (
     ClipAction,
@@ -13,39 +14,42 @@ from wrappers import (
 
 import jax
 import jax.numpy as jnp
-from models import ActorCritic
+from models import ActorCritic, load_model
 
 # -------- Reconstruct environment --------
 
 mode_order = [0, 1, 2, 3]
-mode_switch_steps = 800
+mode_switch_steps = 1000
 episode_length = mode_switch_steps * len(mode_order)
 
-env = InvertedDoublePendulumGymnaxWrapper(
-    mode_switch_steps=mode_switch_steps, switch_order=mode_order
-)
+env = InvertedDoublePendulumGymnaxWrapper()
 env = ClipAction(env)
 
 # -------- Load trained policy --------
 model = ActorCritic(action_dim=env.action_space(None).shape[0], activation="tanh")
-params_path = pathlib.Path("final_policy.msgpack")
-params = from_bytes(
-    model.init(jax.random.PRNGKey(0), jnp.zeros(env.observation_space(None).shape)),
-    params_path.read_bytes(),
+
+params, obs_mean, obs_var = load_model(
+    "logs/distinctive-waterfall-276_final_policy.zip", model, jax.random.PRNGKey(0), env
 )
 
+# model = ActorCritic(action_dim=env.action_space(None).shape[0], activation="tanh")
+# params_path = pathlib.Path("final_policy.msgpack")
+# params = from_bytes(
+#     model.init(jax.random.PRNGKey(0), jnp.zeros(env.observation_space(None).shape)),
+#     params_path.read_bytes(),
+# )
 
-obs_dim = env.observation_space(None).shape[0]
-norm_params_path = pathlib.Path("norm_params.msgpack")
-with norm_params_path.open("rb") as f:
-    data = f.read()
-empty = (jnp.zeros(obs_dim), jnp.ones(obs_dim))
-obs_mean, obs_var = from_bytes(empty, data)
+
+# obs_dim = env.observation_space(None).shape[0]
+# norm_params_path = pathlib.Path("norm_params.msgpack")
+# with norm_params_path.open("rb") as f:
+#     data = f.read()
+# empty = (jnp.zeros(obs_dim), jnp.ones(obs_dim))
+# obs_mean, obs_var = from_bytes(empty, data)
 
 # --- Rollout ---
 rng = jax.random.PRNGKey(0)
-n_episodes = 1
-episode_length = 800  # or env_cfg.episode_length if you have it
+n_episodes = 5
 
 frames = []
 rewards = []
@@ -63,6 +67,9 @@ def run_rollout(rng, params, episode_length):
 
         pi, _ = model.apply(params, obs)
         action = pi.mean()
+        # Sample zero action
+        # action = jnp.zeros_like(action)
+        # action = pi.sample(seed=key)
         obs, next_state, reward, done, info = env.step(key, state, action)
         carry = (rng, next_state)
         return carry, (next_state, reward, done)
@@ -95,21 +102,24 @@ def unbatch_states(batched_states):
 
 
 # --- Execute rollout ---
-rng = jax.random.PRNGKey(200)
-episode_length = 2400
-states, rewards, dones = run_rollout(rng, params, episode_length)
+for i in range(n_episodes):
+    print(f"Episode {i + 1}/{n_episodes}")
 
+    # Use numpy random key to generate a new random key
+    rng = jax.random.PRNGKey(np.random.randint(0, 2**32 - 1))
+    episode_length = 1600
+    states, rewards, dones = run_rollout(rng, params, episode_length)
 
-state_list = unbatch_states(states)
+    state_list = unbatch_states(states)
 
-# --- Find termination point (first done=True)
+    # --- Find termination point (first done=True)
 
-# --- Render rollout ---
-render_every = 1
-frames = [env.render(s) for s in state_list[::render_every]]
+    # --- Render rollout ---
+    render_every = 1
+    frames = [env.render(s) for s in state_list[::render_every]]
 
-# --- Save video ---
-video_path = pathlib.Path("rollout.mp4")
-imageio.mimsave(video_path, frames, fps=40)
-print(f"Saved rollout to {video_path}")
-print(f"Total return: {sum(rewards):.2f}, Episode length: {len(rewards)}")
+    # # --- Save video ---
+    video_path = pathlib.Path(f"rollout_{i}.mp4")
+    imageio.mimsave(video_path, frames, fps=20)
+    print(f"Saved rollout to {video_path}")
+    print(f"Total return: {sum(rewards):.2f}, Episode length: {len(rewards)}")
